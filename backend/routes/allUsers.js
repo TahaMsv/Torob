b57bac4da2cdb23f2e08fc8a272d87ca80e07e74
tahamousavi <tahamousavi.sbu@gmail.com>
2022-07-02 01:09:18 +0430

    All users route tested

--- backend/routes/allUsers.js
+++ backend/routes/allUsers.js
@@ -6,13 +6,13 @@ const error = require("../utilities/errorFunction");
 const authorization = require('../middlewares/user-auth');
 
 
-router.put('/update', authorization,async  function(req, res, next) {
+router.put('/update', authorization, async function (req, res, next) {
     const { email, name, phone, password } = req.body;
     if (!name || !email || !password || !phone) {
         return error(res, "Name, email, phone or password is empty", 400);
     }
     const existUser = await (User.findOne({ email }));
-    if(existUser) return error(res, "Email already exist", 401);
+    if (existUser && email != req.user.email) return error(res, "Email already exist", 401);
 
     const user = await (User.findOne({ emial: req.user.email }));
     user.name = name;
@@ -20,8 +20,8 @@ router.put('/update', authorization,async  function(req, res, next) {
     user.phone = phone;
     user.password = password;
     user.save();
-    
-    return res.status(200).json({  message: "successful" });
+
+    return res.status(200).json({ message: "successful" });
 });
 
 module.exports = router;
