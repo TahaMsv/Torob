c2cfd27d7d0ecfc88800cf17a40b64821fe3aeab
sabaebrahimi <saba.eb18@gmail.com>
2022-07-01 22:53:23 +0430

    delete console logs

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -10,20 +10,16 @@ const NormalUser = mongoose.model('NormalUser');
 router.get('/', authorization, async function (req, res, next) {
     const { value, sortby, type } = req.query;
     const user = await (NormalUser.findOne({ email: req.user.email }));
-    console.log("here13");
     let products;
     if (value) {
         products = await (Product.find({ "name": { "$regex": value, "$options": "i" } }));
     } else if (type) {
         products = await (Product.find({ "type": { "$regex": type, "$options": "i" } }));
     }
-    console.log("here22");
 
     let returndList;
     if (products) {
-        console.log("here26");
         returndList = products.map(product => {
-            console.log("here28");
 
             let leastPrice = 1.797693134862315E+308;
             product.stores.map(store =>{
@@ -32,13 +28,10 @@ router.get('/', authorization, async function (req, res, next) {
             
             let isFavorited = false;
             user.favoriteProducts.map(p => {
-                console.log("here32");
                 if (p === product.id) {
-                    console.log("here34");
                     isFavorited = true;
                 }
             });
-            console.log("here38");
      
             return {
                 id: product.id,
@@ -51,22 +44,16 @@ router.get('/', authorization, async function (req, res, next) {
         });
     }
 
-    console.log("here49");
     console.log(sortby);
     if (returndList) {
-        console.log("here51");
         if (sortby === "newest") {
-            console.log("here53");
             returndList = returndList.sort((p1, p2) => p2.id - p1.id);
         } else if (sortby === "cheap") {
-            console.log("here56");
             returndList = returndList.sort((p1, p2) => p1.leastPrice - p2.leastPrice);
         } else if (sortby === "expensive") {
-            console.log("here59");
             returndList = returndList.sort((p1, p2) => p2.leastPrice - p1.leastPrice);
         }
     }
-    console.log("here62");
     return res.status(200).json(returndList);
 });
 
